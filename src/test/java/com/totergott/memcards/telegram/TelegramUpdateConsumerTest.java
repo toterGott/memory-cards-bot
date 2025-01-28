2d3e6f957e2d23aeffa00e5477c22d1f1e83cd70
a.knyazev <a.knyazev@evotor.ru>
2025-01-28 09:39:21 +0100

    create test with verify

--- src/test/java/com/totergott/memcards/telegram/TelegramUpdateConsumerTest.java
+++ src/test/java/com/totergott/memcards/telegram/TelegramUpdateConsumerTest.java
@@ -1,16 +1,34 @@
 package com.totergott.memcards.telegram;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import com.totergott.memcards.BaseTest;
+import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.context.TestConstructor.AutowireMode;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
 @SpringBootTest
+@TestConstructor(autowireMode = AutowireMode.ALL)
+@AllArgsConstructor
 class TelegramUpdateConsumerTest extends BaseTest {
+
+    private TelegramUpdateConsumer telegramUpdateConsumer;
+
+    @MockitoSpyBean
+    private TelegramUpdateHandler messageHandler;
 
     @Test
     public void when() {
-        assertThat(true).isTrue();
+        var update = new Update();
+
+        telegramUpdateConsumer.consume(update);
+
+        verify(messageHandler, times(1)).handleUpdate(eq(update));
     }
 }
